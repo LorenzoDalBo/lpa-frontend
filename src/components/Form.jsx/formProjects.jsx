0272cf881f4b49f8dea607866fb688590a11c7c3
Eduardo Ribarski <eduardoribarski14@gmail.com>
2023-06-14 14:49:16 -0300

    primeiro commit cards

---
+++ src/components/Form.jsx/formProjects.jsx
@@ -0,0 +1,21 @@
+
+
+export default function FormProjects(){
+return(
+    <div className={`
+        border rounded-md p-5
+    
+    
+    
+        `}>
+        <div>   
+            buscador
+        </div>
+
+        <div>
+            pergunta
+        </div>
+     </div>
+)
+
+}
--- src/pages/projectsPage.jsx
+++ src/pages/projectsPage.jsx
@@ -1,15 +1,31 @@
+import FormProjects from '@/components/Form.jsx/formProjects'
 import React from 'react'
 import { Card, CardGroup, Container, Row } from 'react-bootstrap'
+import CardBase from "@/components/Cards/CardBase";
 
 const ProjectsPage = () => {
 
   
   return (
-    <Container>
+     <Container>
+      <main className='h-fit bg-gray-500'>
         <Row>
-
-        </Row>
-    </Container>
+          <div className='flex-col justify-center items-center gap-6 mt-4'>
+              <div className='flex gap-3'>
+                <FormProjects />
+                <FormProjects />
+                <FormProjects />
+                <FormProjects />  
+                </div>   
+                <div className='flex gap-3'>
+                <CardBase/>
+                <CardBase/>
+                <CardBase/>   
+                </div>     
+            </div>
+           </Row>
+           </main>
+      </Container>
   )
 }
 
