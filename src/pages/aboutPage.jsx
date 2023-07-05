bc8e7b049ead5f29af3f9ced273c2a06bcd572a8
Jean Guichard <jeancharles.g.guichard@gmail.com>
2023-07-05 15:23:12 -0300

    push do edu

--- src/pages/aboutPage.jsx
+++ src/pages/aboutPage.jsx
@@ -9,7 +9,7 @@ const AboutPage = () => {
   return (
     <main className="bg-white py-4">
       <div className="flex justify-center">
-        <div className="flex justify-center items-end rounded-2xl bg-bluesatc4 opacity-80 h-[60px] w-[600px] m-3 p-3">
+        <div className="flex justify-center items-end rounded-2xl bg-bluesatc4 bg-opacity-80 h-[60px] w-[600px] m-3 p-3">
           <h1 className="mt-4 text-2xl text-black">
             <strong>Conheça nossos orientadores:</strong>
           </h1>
@@ -22,11 +22,11 @@ const AboutPage = () => {
               <CardAbout></CardAbout>
             </Col>
             <Col className="text-black flex flex-col justify-center">
-              <div className="bg-bluesatc4 opacity-80 rounded-xl p-3 h-48 w-[600px] text-justify">
+              <div className="bg-bluesatc4 bg-opacity-50 rounded-xl p-3 h-48 w-[600px] text-justify">
                 <h2 className="text-black text-3xl">
                   <strong>Vagner Rodrigues</strong>
                 </h2>
-                <p className="text-black">
+                <p className="text-black mt-2">
                   Nosso Coordenador Adjunto das engenharias de Software e
                   Computação, a pessoa mais gente boa da UNISATC! Possui uma
                   mente incrível de gestão.
@@ -43,11 +43,11 @@ const AboutPage = () => {
           </Row>
           <Row>
             <Col className="text-black flex flex-col justify-center">
-              <div className="bg-bluesatc4 opacity-80 rounded-xl p-3 h-48 w-[600px] text-justify mt-6">
+              <div className="bg-bluesatc4 bg-opacity-50 rounded-xl p-3 h-52 w-[600px] text-justify mt-6">
                 <h2 className="text-black text-3xl">
                   <strong>Anderson Rodrigo Farias</strong>
                 </h2>
-                <p className="text-black">
+                <p className="text-black mt-2">
                   O Coordenador Geral das engenharias de Software e da
                   Computação, uma das pessoas mais sábias e cheio de ideias
                   sobre os projetos! A pessoa da prática, repleto de
@@ -69,7 +69,7 @@ const AboutPage = () => {
         </Container>
       </div>
       <div className="flex justify-center mt-28">
-        <div className="flex justify-center items-end rounded-2xl bg-bluesatc4 opacity-80 h-[60px] w-[600px] m-3 p-3">
+        <div className="flex justify-center items-end rounded-2xl bg-bluesatc4 bg-opacity-80 h-[60px] w-[600px] m-3 p-3">
           <h1 className="mt-4 text-2xl text-black">
             <strong>Sobre o Laboratório LabTec:</strong>
           </h1>
@@ -82,11 +82,11 @@ const AboutPage = () => {
               <CardAboutLab />
             </Col>
             <Col className="text-black flex flex-col justify-center">
-              <div className="bg-bluesatc4 opacity-80 rounded-xl p-3 h-32 w-[600px] text-justify">
+              <div className="bg-bluesatc4 bg-opacity-50 rounded-xl p-3 h-32 w-[600px] text-justify">
                 <h2 className="text-black text-3xl">
                   <strong>LABTEC</strong>
                 </h2>
-                <p className="text-black">
+                <p className="text-black mt-2">
                   Nosso laboratório de pesquisa e desenvolvimento de Software e
                   Computação onde tudo pode surgir, é administrado pelos nossos
                   orientadores ali de cima!
@@ -96,24 +96,27 @@ const AboutPage = () => {
           </Row>
           <Row>
             <Col className="text-black flex flex-col justify-center">
-              <div className="bg-bluesatc4 opacity-80 rounded-xl p-3 h-32 w-[600px] text-justify mt-6">
+              <div className="bg-bluesatc4 bg-opacity-50 rounded-xl p-3 h-44 w-[600px] text-justify mt-6">
                 <h2 className="text-black text-3xl">
                   <strong>Objetivo</strong>
                 </h2>
-                <p className="text-black">
-                  Desenvolver e impulsionar projetos, de forma que sejam criados
-                  para solucionar problemas reais.
+                <p className="text-black mt-2">
+                  O Laboratório possui dois objetivos: acelerar o processo de
+                  colocação dos acadêmicos no mercado, buscando desenvolver
+                  projetos que resolvam problemas reais e, subsidiar as empresas
+                  por meio de pesquisa ou de desenvolvimento de serviços de
+                  acordo com as suas necessidades.
                 </p>
               </div>
             </Col>
             <Col className="flex justify-center">
-              <CardAboutLab2 />
+              <CardAboutLab2/>
             </Col>
           </Row>
         </Container>
       </div>
       <div className="flex justify-center mt-28">
-        <div className="flex justify-center items-end rounded-2xl bg-bluesatc4 opacity-80 h-[60px] w-[600px] m-3 p-3">
+        <div className="flex justify-center items-end rounded-2xl bg-bluesatc4 bg-opacity-80 h-[60px] w-[600px] m-3 p-3">
           <h1 className="mt-4 text-2xl text-black">
             <strong>Desenvolvedores do projeto:</strong>
           </h1>
@@ -122,7 +125,7 @@ const AboutPage = () => {
       <div className="ml-[545px] mt-20">
         <Container className="bg-white">
           <Row>
-            <div className="bg-bluesatc4 opacity-80 rounded-xl p-3 h-36 w-[600px] text-justify">
+            <div className="bg-bluesatc4 bg-opacity-50 rounded-xl p-3 h-36 w-[600px] text-justify">
               <h2 className="text-black text-3xl">
                 <strong>Eduardo Ribarski Scalco</strong>
               </h2>
@@ -130,13 +133,14 @@ const AboutPage = () => {
               <p className="mt-2 text-black">
                 <strong>Acompanhe seu trabalho:</strong> <br/>
                 <a href="https://www.linkedin.com/in/eduardo-ribarski-a691b8250/">
-                  Linkedn - https://www.linkedin.com/in/eduardo-ribarski-a691b8250/
+                  Linkedn -
+                  https://www.linkedin.com/in/eduardo-ribarski-a691b8250/
                 </a>
               </p>
             </div>
           </Row>
           <Row>
-            <div className="bg-bluesatc4 opacity-80 rounded-xl p-3 h-36 w-[600px] text-justify mt-6">
+            <div className="bg-bluesatc4 bg-opacity-50 rounded-xl p-3 h-36 w-[600px] text-justify mt-6">
               <h2 className="text-black text-3xl">
                 <strong>Jean Charles Guichard Guichard</strong>
               </h2>
@@ -150,7 +154,7 @@ const AboutPage = () => {
             </div>
           </Row>
           <Row>
-            <div className="bg-bluesatc4 opacity-80 rounded-xl p-3 h-36 w-[600px] text-justify mt-6">
+            <div className="bg-bluesatc4 bg-opacity-50 rounded-xl p-3 h-36 w-[600px] text-justify mt-6">
               <h2 className="text-black text-3xl">
                 <strong>Lorenzo Trevizol Dal Bó</strong>
               </h2>
@@ -158,7 +162,8 @@ const AboutPage = () => {
               <p className="mt-2 text-black">
                 <strong>Acompanhe seu trabalho:</strong> <br />
                 <a href="https://www.linkedin.com/in/lorenzo-dal-b%C3%B3-7710b4239/">
-                  Linkedn - https://www.linkedin.com/in/lorenzo-dal-b%C3%B3-7710b4239/
+                  Linkedn -
+                  https://www.linkedin.com/in/lorenzo-dal-b%C3%B3-7710b4239/
                 </a>
               </p>
             </div>
