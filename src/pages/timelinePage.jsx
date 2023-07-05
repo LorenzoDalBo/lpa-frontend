435885f10c1368df156243ed1288d17fd272f1b6
LorenzoDalBo <lorenzotrevizoldalbo@gmail.com>
2023-07-05 01:05:15 -0300

    Primeiro merge realizado.

--- src/pages/timelinePage.jsx
+++ src/pages/timelinePage.jsx
@@ -1,15 +1,19 @@
 import Modal from "@/components/Projetos/Modal";
-import React, {useState} from "react";
 import TimelineItem from "@/components/Timeline/timelineItem";
 import {Button, Container, Dropdown, DropdownButton, Form, InputGroup, Row} from "react-bootstrap";
+import React, { useState, useEffect } from "react";
+import { Container, Dropdown, DropdownButton, Form, InputGroup, Row, Pagination } from "react-bootstrap";
 import { VerticalTimeline } from "react-vertical-timeline-component";
 import "react-vertical-timeline-component/style.min.css";
+import TimelineItem from "@/components/Timeline/timelineItem";
+import { times } from "@/styles/times";
+
 
 const TimelinePage = () => {
-
-  const [cidadeFiltrada, setCidadeFiltrada] = useState('');
-
   const [timelineElements, setTimelineElements] = useState([]);
+  const [selectedMonth, setSelectedMonth] = useState("");
+  const [currentPage, setCurrentPage] = useState(1); 
+  const itemsPerPage = 10; 
 
   const handleAddElement = () => {
     
@@ -18,13 +22,54 @@ const TimelinePage = () => {
       ...prevElements,
       <TimelineItem  key={prevElements.length} />,
     ]);
+  useEffect(() => {
+  setTimelineElements(times);
+  }, []);
+
+  const handleFilterYear = (month) => {
+    setSelectedMonth(month);
+    setCurrentPage(1);
   };
 
+  const filteredTimelineElements = timelineElements.filter((element) => {
+    if (selectedMonth === "") {
+      return true;
+    } else {    
+      return element.mes === selectedMonth;
+    }
+  });
 
+  const indexOfLastItem = currentPage * itemsPerPage;
+  const indexOfFirstItem = indexOfLastItem - itemsPerPage;
+  const currentItems = filteredTimelineElements.slice(indexOfFirstItem, indexOfLastItem);
+  const totalPages = Math.ceil(filteredTimelineElements.length / itemsPerPage);
+
+  const handlePageChange = (pageNumber) => {
+    setCurrentPage(pageNumber);
+  };
 
   return (
     <main className="bg-white h-fit w-screen">
       <Container className="bg-slate-200" fluid>
+        <Form className="justify-center flex">
+          <InputGroup className="w-1/3 center justify-center">
+            <DropdownButton variant="outline-secondary" title="Filtrar:" id="input-group-dropdown-1">
+              <Dropdown.Item onClick={() => handleFilterYear("")}>Todos</Dropdown.Item>
+              <Dropdown.Item onClick={() => handleFilterYear("Janeiro")}>Janeiro</Dropdown.Item>
+              <Dropdown.Item onClick={() => handleFilterYear("Fevereiro")}>Fevereiro</Dropdown.Item>
+              <Dropdown.Item onClick={() => handleFilterYear("Março")}>Março</Dropdown.Item>
+              <Dropdown.Item onClick={() => handleFilterYear("Abril")}>Abril</Dropdown.Item>
+              <Dropdown.Item onClick={() => handleFilterYear("Maio")}>Maio</Dropdown.Item>
+              <Dropdown.Item onClick={() => handleFilterYear("Junho")}>Junho</Dropdown.Item>
+              <Dropdown.Item onClick={() => handleFilterYear("Julho")}>Julho</Dropdown.Item>
+              <Dropdown.Item onClick={() => handleFilterYear("Agosto")}>Agosto</Dropdown.Item>
+              <Dropdown.Item onClick={() => handleFilterYear("Setembro")}>Setembro</Dropdown.Item>
+              <Dropdown.Item onClick={() => handleFilterYear("Outubro")}>Outubro</Dropdown.Item>
+              <Dropdown.Item onClick={() => handleFilterYear("Novembro")}>Novembro</Dropdown.Item>
+              <Dropdown.Item onClick={() => handleFilterYear("Dezembro")}>Dezembro</Dropdown.Item>
+            </DropdownButton>
+          </InputGroup>
+        </Form>
         <Row>
           <Form className="flex border-t-2 border-solid border-bluesatc3 justify-center p-3">
             <InputGroup className="w-1/3">
@@ -52,19 +97,18 @@ const TimelinePage = () => {
           </div>COMENTÁRIO JSX */}  
         </Row>
         <Row>
-          <VerticalTimeline className="mt-3 line-black">
-          <div className="timeline">
-            {timelineElements.map((element) => element)}
-            <TimelineItem/>
-            <TimelineItem/>
-            <TimelineItem/>
-          </div>
+          <VerticalTimeline className="" lineColor="#000000">
+            <div className="timeline">
+              {currentItems.map((element, index) => (
+                <TimelineItem key={index} timesItem={element} />
+              ))}
+            </div>
           </VerticalTimeline>
-          
         </Row>
       </Container>
     </main>
   );
 };
+
 
 export default TimelinePage;
